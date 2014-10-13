3ba9d328a055a6d7a439bfe0fb05fe5d02fec78a
Steven Schlansker <sschlansker@opentable.com>
2014-10-13 10:11:59 -0700

    0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="nagios-mesos-service-check",
     description="A selection of Nagios plugins to monitor services hosted in OpenTable Mesos.",
     long_description=open('README.rst').read(),
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author='Steven Schlansker',
     author_email='sschlansker@opentable.com',
