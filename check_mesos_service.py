eeeaea2f215343023ee46c896daac95330ae9c0a
Steven Schlansker <sschlansker@opentable.com>
2015-02-27 10:21:58 -0800

    Merge discovery-state back into the script, since packaging it is a pain

--- check_mesos_service.py
+++ check_mesos_service.py
@@ -4,12 +4,41 @@ import argparse
 import logging
 import re
 import requests
-from discovery_state import DiscoveryState
 
 log = logging.getLogger("nagiosplugin")
+
 INFINITY = float('inf')
 HEALTHY = 1
 UNHEALTHY = -1
+TOKEN = "server-token"
+
+class DiscoveryState(nagiosplugin.Resource):
+  def __init__(self, name, announcements):
+    self.myname = name
+    self.announcements = announcements
+
+  @property
+  def name(self):
+    return self.myname + ' announcement'
+
+  def probe(self):
+    seen = set()
+    count = 0
+    for ann in self.announcements:
+      metadata = ann.get("metadata", dict())
+      if TOKEN in metadata:
+        token = metadata[TOKEN]
+        if token not in seen:
+          seen.add(token)
+          log.debug('New token %s' % token)
+          count += 1
+        else:
+          log.debug('Seen token %s' % token)
+      else:
+        log.debug('No token for service %s' % ann['announcementId'])
+        count += 1
+    yield nagiosplugin.Metric('announced services', count)
+
 
 class MesosHealthCheck(nagiosplugin.Resource):
   def __init__(self, name, service_uri, endpoint, metric_name, timeout):
